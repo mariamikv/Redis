f72b9d9cab840b03bb229951ff803de9e2ff930c
mariamikv <mariamikv71@gmail.com>
2024-11-07 23:57:47 +0400

    Handle concurrent clients

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,43 @@
+import kotlinx.coroutines.*
 import java.net.ServerSocket
+import java.net.Socket
 
-fun main(args: Array<String>) {
+fun main() = runBlocking  {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!")
 
     val serverSocket = ServerSocket(6379)
-    val client = serverSocket.accept()
-
-    val input = client.getInputStream()
-    val output = client.getOutputStream()
-
-    println("accepted new connection")
 
     while (true) {
-        val request = input.bufferedReader()
-        val requestBody = request.readLine().orEmpty()
-        if(requestBody.isEmpty()) break
+        try {
+            val client = serverSocket.accept()
 
-        output.write("+PONG\r\n".toByteArray())
-        output.flush()
+            println("accepted new connection")
+
+            launch(Dispatchers.IO) {
+                handleClient(client = client)
+            }
+        }
+        catch (e : Exception) {
+            break
+        }
+    }
+    serverSocket.close()
+}
+
+suspend fun handleClient(client: Socket) {
+    withContext(Dispatchers.IO) {
+        val input = client.getInputStream()
+        val output = client.getOutputStream()
+
+        while (true) {
+            val request = input.bufferedReader()
+            val requestBody = request.readLine().orEmpty()
+            if (requestBody.isEmpty()) break
+            println("request received: $requestBody")
+
+            output.write("+PONG\r\n".toByteArray())
+            output.flush()
+        }
     }
 }
